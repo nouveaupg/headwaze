4e0dd8744c1c7a43a1f1eb6e5e806e5df4cb8eb3
John Hill <rapidiphonedev@gmail.com>
2017-12-12 09:31:33 +0000

    templates finally working

--- service_endpoint.py
+++ service_endpoint.py
@@ -8,9 +8,11 @@ app = Flask(__name__)
 INVALID_REQUEST = "{\"success\":false,\"msg\":\"Invalid UUID.\"}"
 UUID_RE = re.compile("[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}")
 
+NEARBY_STOPS = ['Canal St', 'Franklin St', 'Chambers St', 'Cortlandt St', 'Rector St', 'South Ferry Loop', 'South Ferry', 'Park Pl', 'Fulton St', 'Wall St', 'Bowling Green', 'Spring St', 'Brooklyn Bridge - City Hall', 'Broadway-Lafayette St', 'World Trade Center', 'Broad St', 'Prince St', 'City Hall', 'Whitehall St']
+
 @app.route('/')
 def home():
-    return render_template("main.html")
+    return render_template("main.html",nearby_stations=NEARBY_STOPS)
 
 @app.route('/stations-near/<uuid>')
 def stations_near(uuid):
